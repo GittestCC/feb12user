4b3fb49efa928a97f8afde6ff41752f560d745d9
Nadeem Khedr <nadeemkhedr@gmail.com>
2018-03-14 17:06:54 +0800

    💖 fix(kintoBlocks): remove coming from custom params
    GREAT!

--- src/components/dashboard/kintoBlocks/kintoBlockManage/KintoBlockManageParamsField.js
+++ src/components/dashboard/kintoBlocks/kintoBlockManage/KintoBlockManageParamsField.js
@@ -49,7 +49,6 @@ class KintoBlockManageParamsField extends Component {
     return (
       <div className="form-body custom-params" data-test="kb-manage-params">
         <div className="top">
-          <h6 className="coming">Coming</h6>
           <h4 className="title">Custom Parameters</h4>
           {fields.length ? (
             fields.map((field, index) => (
